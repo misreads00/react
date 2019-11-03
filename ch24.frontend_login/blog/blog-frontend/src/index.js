d09499f4e58684dee24388f221feb5be6bfa132c
jiyoonhong <misreads00@gmail.com>
2019-11-03 17:51:05 +0900

    ch.24 updata

---
+++ ch24.frontend_login/blog/blog-frontend/src/index.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+import { BrowserRouter } from 'react-router-dom';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import rootReducer, { rootSaga } from './modules';
+import createSagaMiddleware from 'redux-saga';
+import { tempSetUser, check } from './modules/user';
+
+const sagaMiddleware = createSagaMiddleware();
+const store = createStore(
+    rootReducer, 
+    composeWithDevTools(applyMiddleware(sagaMiddleware))
+);
+
+function loadUser() {
+    try {
+      const user = localStorage.getItem('user');
+      if (!user) return; // 로그인 상태가 아니라면 아무것도 안함
+  
+      store.dispatch(tempSetUser(user));
+      store.dispatch(check());
+    } catch (e) {
+      console.log('localStorage is not working');
+    }
+  }
+
+//꼭 run 이후에 loadUser 해야함. 
+//(check action 을 dispatch 했을떄, saga에서 이를 제대로 처리하기 위해)
+sagaMiddleware.run(rootSaga);
+loadUser();
+
+ReactDOM.render(
+    <Provider store={store}>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </Provider>, 
+    document.getElementById('root')
+);
+
+
+serviceWorker.unregister();
