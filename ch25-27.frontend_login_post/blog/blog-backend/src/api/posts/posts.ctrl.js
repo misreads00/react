4bc62f7b2c41f90d4fd42e0c9a62e78699c21e76
jiyoonhong <misreads00@gmail.com>
2019-11-05 19:50:11 +0900

    ch26 updated(b)

--- ch25-27.frontend_login_post/blog/blog-backend/src/api/posts/posts.ctrl.js
+++ ch25-27.frontend_login_post/blog/blog-backend/src/api/posts/posts.ctrl.js
@@ -2,8 +2,36 @@
 import Post from '../../models/post';
 import mongoose from 'mongoose';
 import Joi from 'joi';
+import sanitizeHtml from 'sanitize-html';
 
 const { ObjectId } = mongoose.Types; 
+
+const sanitizeOption = {
+    allowedTags: [
+      'h1',
+      'h2',
+      'b',
+      'i',
+      'u',
+      's',
+      'p',
+      'ul',
+      'ol',
+      'li',
+      'blockquote',
+      'a',
+      'img',
+    ],
+    allowedAttributes: {
+      a: ['href', 'name', 'target'],
+      img: ['src'],
+      li: ['class'],
+    },
+    allowedSchemes: ['data', 'http'],
+  };
+
+
+
 
 export const getPostById = async (ctx, next) => {
     const { id } = ctx.params;
@@ -59,7 +87,7 @@ export const write = async ctx => {
     const { title, body, tags } = ctx.request.body;
     const post = new Post({
         title, 
-        body, 
+        body:sanitizeHtml(body, sanitizeOption),
         tags, 
         user: ctx.state.user, 
     });
@@ -72,6 +100,13 @@ export const write = async ctx => {
 };
 
 
+
+const removeHtmlAndShorten = body => {
+    const filtered = sanitizeHtml(body, {
+        allowedTags: [],
+    });
+    return filtered.length < 200 ? filtered : `${filtered.slice(0,200)}`; 
+};
 
 // GET /api/posts 
 export const list = async ctx => {
@@ -102,8 +137,7 @@ export const list = async ctx => {
         //200자 초과할경우 생략하도록 표기. 
         ctx.body = posts.map(post => ({
             ...post,
-            body: 
-                post.body.length < 200 ? post.body : `${post.body.slice(0,200)}...`, 
+            body: removeHtmlAndShorten(post.body),
         }));
     } catch (e) {
         ctx.throw(500, e);
@@ -151,8 +185,14 @@ export const update = async ctx => {
         return;
     }
 
+    // 객체를 복사하고 + body 값이 주어졌으면 sanitizeHtml로 HTML 필터링
+    const nextData = { ...ctx.request.body }; 
+    if (nextData.body ) {
+        nextData.body = sanitizeHtml(nextData.body);
+    }
+
     try {
-        const post = await Post.findByIdAndUpdate(id, ctx.request.body, {
+        const post = await Post.findByIdAndUpdate(id, nextData, {
             new: true, //이값을 설정하면 업데이트된 데이터를 반환. false일땐 업데이트전 데이터. 
         }).exec();
         if (!post) {
